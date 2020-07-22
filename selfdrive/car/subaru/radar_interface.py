82b5da637de701f57776ef3853f4f7dbade21359
Michael Honan <mc.honan1996@gmail.com>
2020-07-22 19:51:23 +1000

    Car types for radar interface

--- selfdrive/car/subaru/radar_interface.py
+++ selfdrive/car/subaru/radar_interface.py
@@ -7,19 +7,22 @@ from selfdrive.car.interfaces import RadarInterfaceBase
 from selfdrive.car.subaru.values import DBC
 
 def get_eyesight_can_parser(CP):
-  signals = [
-    # sig_name, sig_address, default
-    ("Car_Follow", "ES_DashStatus", 0),
-    ("Far_Distance", "ES_DashStatus", 0),
+  signals = []
+  checks = []
+  if CP.carFingerprint not in PREGLOBAL_CAR:
+    signals += [
+      # sig_name, sig_address, default
+      ("Car_Follow", "ES_DashStatus", 0),
+      ("Far_Distance", "ES_DashStatus", 0),
 
-    ("Close_Distance", "ES_Distance", 0),
-    ("Distance_Swap", "ES_Distance", 0),
-  ]
-  checks = [
-    # address, frequency
-    ("ES_DashStatus", 10),
-    ("ES_Distance", 20),
-  ]
+      ("Close_Distance", "ES_Distance", 0),
+      ("Distance_Swap", "ES_Distance", 0),
+    ]
+    checks += [
+      # address, frequency
+      ("ES_DashStatus", 10),
+      ("ES_Distance", 20),
+    ]
   return CANParser(DBC[CP.carFingerprint]['pt'], signals, checks, 0)
 
 class RadarInterface(RadarInterfaceBase):
@@ -27,7 +30,8 @@ class RadarInterface(RadarInterfaceBase):
     super().__init__(CP)
     self.canParser = get_eyesight_can_parser(CP)
     self.updated_messages = set()
-    self.trigger_msg = 0x321 # 0x221 - ES_Distance | 0x321 - ES_DashStatus
+    if CP.carFingerprint not in PREGLOBAL_CAR:
+      self.trigger_msg = 0x321 # 0x221 - ES_Distance | 0x321 - ES_DashStatus
     self.track_id = 0
     self.radar_off_can = CP.radarOffCan
 
@@ -57,32 +61,33 @@ class RadarInterface(RadarInterfaceBase):
       errors.append("canError")
     ret.errors = errors
 
-    valid = cpt["ES_DashStatus"]['Car_Follow'] == 1
-    if valid:
-      far_distance = cpt['ES_DashStatus']['Far_Distance']
-      close_distance = cpt['ES_Distance']['Close_Distance']
+    if CP.carFingerprint not in PREGLOBAL_CAR:
+      valid = cpt["ES_DashStatus"]['Car_Follow'] == 1
+      if valid:
+        far_distance = cpt['ES_DashStatus']['Far_Distance']
+        close_distance = cpt['ES_Distance']['Close_Distance']
 
-      self.pts[0] = car.RadarData.RadarPoint.new_message()
-      self.pts[0].trackId = self.track_id
-      self.track_id += 1
-      self.pts[0].dRel = (close_distance * 6 / 255) if close_distance < 255 else (far_distance * 5) # from front of car
-      self.pts[0].yRel = 0 # float('nan')  # in car frame's y axis, left is negative
-      self.pts[0].vRel = float('nan')
-      self.pts[0].aRel = float('nan')
-      self.pts[0].yvRel = float('nan')
-      self.pts[0].measured = True
+        self.pts[0] = car.RadarData.RadarPoint.new_message()
+        self.pts[0].trackId = self.track_id
+        self.track_id += 1
+        self.pts[0].dRel = (close_distance * 6 / 255) if close_distance < 255 else (far_distance * 5) # from front of car
+        self.pts[0].yRel = 0 # float('nan')  # in car frame's y axis, left is negative
+        self.pts[0].vRel = float('nan')
+        self.pts[0].aRel = float('nan')
+        self.pts[0].yvRel = float('nan')
+        self.pts[0].measured = True
 
-      # for ii in range(2):
-      #   if ii not in self.pts:
-      #     self.pts[ii] = car.RadarData.RadarPoint.new_message()
-      #     self.pts[ii].trackId = self.track_id
-      #     self.track_id += 1
-      #   self.pts[ii].dRel = cpt["SCC11"]['ACC_ObjDist']  # from front of car
-      #   self.pts[ii].yRel = -cpt["SCC11"]['ACC_ObjLatPos']  # in car frame's y axis, left is negative
-      #   self.pts[ii].vRel = cpt["SCC11"]['ACC_ObjRelSpd']
-      #   self.pts[ii].aRel = float('nan')
-      #   self.pts[ii].yvRel = float('nan')
-      #   self.pts[ii].measured = True
+        # for ii in range(2):
+        #   if ii not in self.pts:
+        #     self.pts[ii] = car.RadarData.RadarPoint.new_message()
+        #     self.pts[ii].trackId = self.track_id
+        #     self.track_id += 1
+        #   self.pts[ii].dRel = cpt["SCC11"]['ACC_ObjDist']  # from front of car
+        #   self.pts[ii].yRel = -cpt["SCC11"]['ACC_ObjLatPos']  # in car frame's y axis, left is negative
+        #   self.pts[ii].vRel = cpt["SCC11"]['ACC_ObjRelSpd']
+        #   self.pts[ii].aRel = float('nan')
+        #   self.pts[ii].yvRel = float('nan')
+        #   self.pts[ii].measured = True
 
     ret.points = list(self.pts.values())
     return ret
