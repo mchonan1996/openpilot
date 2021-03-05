21c12c9ec56139c41087d57f5a1781f4dce6dba3
Michael Honan <mc.honan1996@gmail.com>
2021-03-05 13:45:31 +1100

    kegman live tune

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -3,17 +3,35 @@ import math
 from selfdrive.controls.lib.pid import PIController
 from selfdrive.controls.lib.drive_helpers import get_steer_max
 from cereal import log
-
+from selfdrive.kegman_conf import kegman_conf
 
 class LatControlPID():
   def __init__(self, CP):
+    self.kegman = kegman_conf(CP)
     self.pid = PIController((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
                             (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
                             k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, neg_limit=-1.0,
                             sat_limit=CP.steerLimitTimer)
+    self.mpc_frame = 0
 
   def reset(self):
     self.pid.reset()
+
+  def live_tune(self, CP):
+    self.mpc_frame += 1
+    if self.mpc_frame % 300 == 0:
+      # live tuning through /data/openpilot/tune.py overrides interface.py settings
+      self.kegman = kegman_conf()
+      if self.kegman.conf['tuneGernby'] == "1":
+        self.steerKpV = [float(self.kegman.conf['Kp'])]
+        self.steerKiV = [float(self.kegman.conf['Ki'])]
+        self.steerKf = float(self.kegman.conf['Kf'])
+        self.pid = PIController((CP.lateralTuning.pid.kpBP, self.steerKpV),
+                            (CP.lateralTuning.pid.kiBP, self.steerKiV),
+                            k_f=self.steerKf, pos_limit=1.0)
+        self.deadzone = float(self.kegman.conf['deadzone'])
+        
+      self.mpc_frame = 0    
 
   def update(self, active, CS, CP, VM, params, lat_plan):
     pid_log = log.ControlsState.LateralPIDState.new_message()
